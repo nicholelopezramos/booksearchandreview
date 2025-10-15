90599a0aa99178c978d5fd07ea6c4fd75c256a6b
nicholelopezramos <nicholelopez587@gmail.com>
2025-10-16 00:56:09 +0800

    added resolve viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     // CRITICAL: Sets the base URL for the build output to relative paths (e.g., ./assets/)
     base: './',
+    resolve: {
+        // This allows Vite to resolve modules like 'fuse.js' correctly
+        // by searching in the 'node_modules' folder.
+        mainFields: ['module', 'jsnext:main', 'jsnext'],
+    }
 });
